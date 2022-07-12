2057d72af3edb3bf32898ac1b313eb8b7b6be51b
kuijunming93 <kuijunming93@gmail.com>
2022-07-12 14:47:46 +0800

    Resolving static files issue

--- projectMail/FoodMail/settings.py
+++ projectMail/FoodMail/settings.py
@@ -122,8 +122,8 @@ STATIC_ROOT = "/home/kuijunming93/djangoProject/projectMail/static"
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static"]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
