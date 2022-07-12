ca6ad2a147a880a9c27b1749efc5441f9df1be95
kuijunming93 <kuijunming93@gmail.com>
2022-07-12 15:53:39 +0800

    Fixed resolution for mobile

--- projectMail/FoodMail/settings.py
+++ projectMail/FoodMail/settings.py
@@ -122,8 +122,9 @@ STATIC_ROOT = "/home/kuijunming93/djangoProject/projectMail/static"
 
 STATIC_URL = 'static/'
 
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static"]
+STATICFILES_DIRS = [
+    BASE_DIR / "static"]
+# NEED TO COMMENT ABOVE FOR DEPLOYMENT
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
