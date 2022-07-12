c9863cc74558d20256882b8c79e6d75ab4b2f8a7
kuijunming93 <kuijunming93@gmail.com>
2022-07-12 16:13:24 +0800

    Recycle

--- projectMail/FoodMail/settings.py
+++ projectMail/FoodMail/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = 'static/'
 
 # STATICFILES_DIRS = [
 #     BASE_DIR / "static"]
-# NEED TO COMMENT ABOVE FOR DEPLOYMENT
+# NEED TO COMMENT ABOVE FOR DEPLOYMENT ONLY
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
