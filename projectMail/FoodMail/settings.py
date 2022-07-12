b8689f93959e9d5eb77c71c961fb55861ace72ff
kuijunming93 <kuijunming93@gmail.com>
2022-07-12 16:18:20 +0800

    Direct texture file reference to GH

--- projectMail/FoodMail/settings.py
+++ projectMail/FoodMail/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = 'static/'
 
 # STATICFILES_DIRS = [
 #     BASE_DIR / "static"]
-# NEED TO COMMENT ABOVE FOR DEPLOYMENT ONLY
+# NEED TO COMMENT ABOVE FOR DEPLOYMENT
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
